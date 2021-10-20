eeb81d955188742b8764f1cee26d3a9ff4683e30
Severyn <severynson.kurach@gmail.com>
2021-10-21 00:22:05 +0300

    LogIn popup closing button works  but no full optimization of size

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 'use strict';
 
-const popupBlock = document.querySelector('popup-log-in-or-sign-in')
+const popupBlock = document.querySelector('.popup-log-in-or-sign-in');
+const closePopupButton = document.querySelector('.close-icon');
+
+const closePopupWindow = () => {
+    popupBlock.style.opacity = "0";
+    popupBlock.style.visibility = "hidden";
+}
+
+closePopupButton.addEventListener('click', closePopupWindow);
