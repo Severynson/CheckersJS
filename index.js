85e69299e6110ade57fd0b2de72cfd4591112100
Severyn <severynson.kurach@gmail.com>
2021-10-21 17:55:23 +0300

    mainMenuDone

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 
 const popupBlock = document.querySelector('.popup-log-in-or-sign-in');
 const closePopupButton = document.querySelector('.close-icon');
+const usernameInput = document.querySelector('.username-input');
+const passwordInput = document.querySelector('.password-input');
+const logInConfirmBtn = document.querySelector('.log-in-confirm-btn');
+const StartNewGameBtn = document.querySelector('.start-new-game-btn');
+const mainMenuNumOne = document.querySelector('.main-menu-n-1');
+
+let userDataBase = [["Severyn", "111"], ["Ivan", "222"], ["Nina", "333"]];
 
 const closePopupWindow = () => {
     popupBlock.style.opacity = "0";
@@ -9,3 +16,32 @@ const closePopupWindow = () => {
 }
 
 closePopupButton.addEventListener('click', closePopupWindow);
+
+const logIn = () => {
+    userDataBase.forEach(function(person, index, pArr) {
+        console.log(person);
+        console.log(index)
+        console.log(userDataBase.length)
+        console.log(person[0]);
+        console.log(person[1]);
+        if (person[0] === usernameInput.value && person[1] === passwordInput.value) {
+            popupBlock.style.opacity = "0";
+    popupBlock.style.visibility = "hidden";
+        } else if (index + 1 === userDataBase.length) {
+            passwordInput.value = '';
+            passwordInput.placeholder = "something isn't correct"
+    
+            usernameInput.value = '';
+            usernameInput.placeholder = "something isn't correct"
+        }
+    })
+}
+
+logInConfirmBtn.addEventListener('click', logIn);
+
+const startNewGame = () => {
+    mainMenuNumOne.style.opacity = "0";
+    mainMenuNumOne.style.visibility = "hidden";
+}
+
+StartNewGameBtn.addEventListener('click', startNewGame);
