6e539c551da744512ff042bfd448b4577c4975e2
Severyn Kurach <severynson.kurach@gmail.com>
2021-11-16 05:01:17 +0200

    fixedBugWithDeletingSameColorChecker

--- CheckersGame/Main.js
+++ CheckersGame/Main.js
@@ -24,44 +24,54 @@ const isCheckerDamka = function () {
   }
 };
 
-// cellsBetween.length
-// cell1Checker.lastChild.style.visibility === "visible"
-
 var visibilityOfStar;
 
 const putChecker = function () {
-    const replace = () => {
+  const replace = () => {
     cell1Checker.parentNode.removeChild(cell1Checker);
-  let cellXChecker = new Checker(
-    clickedDiv,
-    cell1CheckerColor,
-    visibilityOfStar
-  );
-  cellXChecker.createCheckers();
-  }
+    let cellXChecker = new Checker(
+      clickedDiv,
+      cell1CheckerColor,
+      visibilityOfStar
+    );
+    cellXChecker.createCheckers();
+  };
   visibilityOfStar = "hidden";
   isCheckerDamka();
 
-  if (cellsBetween.length === 0 && cell1Checker.lastChild.style.visibility === "hidden") {
+  let cellsBetweenContainsSameColorCheckers = false;
+
+  cellsBetween.forEach((el) => {
+    let cellXChecker = document.querySelector(`.${el}-checker-div-element`);
+    let cellBetweenChecker = document.querySelector(`.${cellXChecker.parentNode.className.split(" ")[0]}-checker-div-element`);
+    console.log(cell1CheckerColor);
+    cellXChecker && console.log(cellBetweenChecker.style.backgroundColor);
+    if (cell1CheckerColor === cellBetweenChecker.style.backgroundColor) {
+      cellsBetweenContainsSameColorCheckers = true;
+    } else {
+      cellsBetweenContainsSameColorCheckers = false;
+    }
+  })
+
+  if (
+    cellsBetween.length === 0 &&
+    cell1Checker.lastChild.style.visibility === "hidden"
+  ) {
     replace();
     return true;
-  } else if (cellsBetween.length === 1 && document.querySelector((`.${cellsBetween[0]}-checker-div-element`)) && cell1Checker.lastChild.style.visibility === "hidden") {
+  } else if (
+    cellsBetween.length === 1 && cellsBetweenContainsSameColorCheckers === false &&
+    document.querySelector(`.${cellsBetween[0]}-checker-div-element`) &&
+    cell1Checker.lastChild.style.visibility === "hidden"
+  ) {
     replace();
     return true;
-////
-/////////
-/////////
-//////////////
-/////////////////
-//////////////////////
-///////////////////////////
-  } else if (cell1Checker.lastChild.style.visibility === "visible") {
+  } else if (cell1Checker.lastChild.style.visibility === "visible"&& cellsBetweenContainsSameColorCheckers === false) {
     replace();
     return true;
   } else {
-    counter = 1
+    counter = 1;
   }
-
 };
 
 var cell1DivIndex;
@@ -71,25 +81,27 @@ var cellsBetween = [];
 
 const secondPartAnaliz = function () {
   let clickedDivID = clickedDiv.className.split(" ")[0];
-  let cell2Checker = document.querySelector(`.${clickedDivID}-checker-div-element`);
+  let cell2Checker = document.querySelector(
+    `.${clickedDivID}-checker-div-element`
+  );
   if (cell2Checker === null || cell2Checker === cell1Checker) {
-      const betweenToDelete = (diagonal) => {
-    stepDiagonal = diagonal;
-    diagonal.forEach((el, i) => {
-      el.includes(cell1ID) && (cell1DivIndex = i);
-    });
-    diagonal.forEach((el, i) => {
-      el.includes(clickedDivID) && (cell2DivIndex = i);
-    });
-  };
+    const betweenToDelete = (diagonal) => {
+      stepDiagonal = diagonal;
+      diagonal.forEach((el, i) => {
+        el.includes(cell1ID) && (cell1DivIndex = i);
+      });
+      diagonal.forEach((el, i) => {
+        el.includes(clickedDivID) && (cell2DivIndex = i);
+      });
+    };
 
-  cell1LeftD.includes(clickedDivID) && betweenToDelete(cell1LeftD);
-  cell1RightD.includes(clickedDivID) && betweenToDelete(cell1RightD);
-  cellsBetween = stepDiagonal.slice(cell1DivIndex + 1, cell2DivIndex);
-  cellsBetween.length === 0 &&
-    (cellsBetween = stepDiagonal.slice(cell2DivIndex + 1, cell1DivIndex));
-    console.log(cellsBetween.length)
-  return true;
+    cell1LeftD.includes(clickedDivID) && betweenToDelete(cell1LeftD);
+    cell1RightD.includes(clickedDivID) && betweenToDelete(cell1RightD);
+    cellsBetween = stepDiagonal.slice(cell1DivIndex + 1, cell2DivIndex);
+    cellsBetween.length === 0 &&
+      (cellsBetween = stepDiagonal.slice(cell2DivIndex + 1, cell1DivIndex));
+    console.log(cellsBetween.length);
+    return true;
   } else {
     counter = 1;
   }
@@ -125,16 +137,15 @@ var diagonalsCell1Classes;
 const analizePosition = function (clickedDiv) {
   cell1ID = clickedDiv.className.split(" ")[0];
   cell1Checker = document.querySelector(`.${cell1ID}-checker-div-element`);
-  // Is checker in cell avalible: 
+  // Is checker in cell avalible:
   if (cell1Checker !== null) {
     cell1CheckerColor = `${cell1Checker.style.backgroundColor}`;
-  cell1Checker.parentNode.lastChild.style.opacity = "0.7";
-  LRDiagonalsCheck(cell1ID);
-  diagonalsCell1Classes = [...cell1LeftD, ...cell1RightD];
+    cell1Checker.parentNode.lastChild.style.opacity = "0.7";
+    LRDiagonalsCheck(cell1ID);
+    diagonalsCell1Classes = [...cell1LeftD, ...cell1RightD];
   } else {
     counter = 0;
   }
-  
 };
 
 let counter = 0;
